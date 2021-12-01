3a895ddb27fe4601a7d0edb6c660cdcfabee61b2
Arthur Chastenet <arthur.chastenet@gmail.com>
2021-12-01 17:52:14 +0100

    Iteration 3 almost done - just 3.3 to finish

--- js/index.js
+++ js/index.js
@@ -1,24 +1,47 @@
 // Iteration 1: Names and Input
 
-var hacker1 = "Alexandra";
+const hacker1 = "Alexandra";
 console.log ("The driver's name is "+ hacker1); 
 
-var hacker2 = "Arthur";
+const hacker2 = "Arthur";
 console.log (`The navigator name is ${hacker2}`); 
-
 
 // Iteration 2: Conditionals
 
 if (hacker1.length>hacker2.length) {
-console.log(`The driver has the longest name, it has ${hacker1.length} characters.`);   
-
-}
-else if (hacker2.length>hacker1.length) {
-console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`); 
-}
-
-else {
-console.log(`Wow, you both have equally long names, ${hacker1.length} characters!`); 
+    console.log(`The driver has the longest name, it has ${hacker1.length} characters.`);
+} else if (hacker2.length>hacker1.length) {
+    console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`); 
+} else {
+    console.log(`Wow, you both have equally long names, ${hacker1.length} characters!`); 
 } 
 
 // Iteration 3: Loopsgit
+
+// 3.1
+
+let i = 0;
+let nameUppercaseHacker1 = "";
+
+while (i < hacker1.length) {;
+    nameUppercaseHacker1 += hacker1[i].toUpperCase() + " ";
+    i++;
+}
+
+console.log(nameUppercaseHacker1);
+
+// 3.2
+
+let nameLowercaseHacker2 = "";
+
+for (let i = hacker2.length -1; i >= 0; i--) {
+    nameLowercaseHacker2 += hacker2[i].toLowerCase();
+}
+
+console.log(nameLowercaseHacker2);
+
+// 3.3
+
+const test = hacker1[1] < hacker2[1];
+console.log(test);
+
